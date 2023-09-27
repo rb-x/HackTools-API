dc58f6e362a16317e38271a4870626e257b99c18
Riadh B <riadh.bch@outlook.fr>
2023-09-27 16:57:59 +0200

    fixing cors + auth via middleware

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends, APIRouter, Request
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import JSONResponse
 import secrets
 from app.routes.windows import sam, ntds, lsass
 from app.services import vulners
 import logging,os
-
+from typing import Optional
 
 logger = logging.getLogger("api_key_logger")
 dev = os.environ.get("DEV")
@@ -23,20 +23,30 @@ app.add_middleware(
 async def on_startup():
     logger.warning(f" | API key: {API_KEY}")
 
-@app.middleware("http")
-async def check_api_key(request, call_next):
-    api_key = request.headers.get("x-api-key")
+def api_key_check(request: Request):
+    api_key = request.headers.get('x-api-key', None)
     if api_key != API_KEY:
-        return JSONResponse(status_code=401, content={"detail": "Invalid API key."})
-    try:
-        response = await call_next(request)
-    except Exception as e:
-        return JSONResponse(status_code=500, content={"detail": "An internal error occurred."})
-    
-    return response
+        raise HTTPException(status_code=401, detail="Invalid API key.")
+    return api_key
+
+class AuthenticatedRouter(APIRouter):
+    def add_api_route(self, *args, **kwargs):
+        if "dependencies" not in kwargs:
+            kwargs["dependencies"] = []
+        kwargs["dependencies"].append(Depends(api_key_check))
+        super().add_api_route(*args, **kwargs)
+
+router = AuthenticatedRouter()
+
+@app.get("/ping")
+async def ping():
+    return {"status": "success"}
+
+@app.get("/auth_check")
+async def auth_check(api_key: str = Depends(api_key_check)):
+    return {"status": "success"}
 
 app.include_router(vulners.router, prefix="/vulners", tags=["Vulners API"])
 app.include_router(sam.router, prefix="/decrypt/sam", tags=["SAM Decryption"])
 app.include_router(ntds.router, prefix="/decrypt/ntds", tags=["NTDS Decryption"])
 app.include_router(lsass.router, prefix="/decrypt/lsass", tags=["LSASS dump"])
- 
